8b4b67e5e52129126a0e1da81c6ebf65af1f79a6
jgammerman <jgammerman@gmail.com>
2020-11-10 18:24:18 +0000

    refactored

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,29 +17,34 @@ class AlienInvasion:
 
 		self.screen = pygame.display.set_mode(
 			(self.settings.screen_width, self.settings.screen_height))  # create a display window, on which we’ll draw all the game’s graphical elements. 1200 pixels wide by 800 pixels high
-		pygame.display.set_caption("Alien Invasion")
+		pygame.display.set_caption("Alien Invasion Game by James Gammerman")
 
 		self.ship = Ship(self) # Make an instance of Ship after the screen has been created - only one argument, the current instance of AlienInvation.
-
-		# Set the background colour
-		self.bg_color = (230, 230, 230) # equal mixture of RGB - produces light grey colour
 
 
 	def run_game(self):
 		"""Start the main loop for the game."""
-		while True:  
-			# Watch for keyboard and mouse events
-			for event in pygame.event.get():  # this function returns a list of events that have taken place since the last time it was called
-				if event.type == pygame.QUIT:  # a pygame.QUIT event is detected if the player clicks the game window's close button
-					sys.exit()
+		while True: 
+			self._check_events()
+			self._update_screen()
 
-			# Redraw the screen during each pass through the loop.
-			self.screen.fill(self.settings.bg_color)	# Fill the screen with the background colour using the fill() method
-			self.ship.blitme() # Draw the ship on the screen so it appears on top of the background	
 
-			# Make the most recently drawn screen visible. pygame.display.flip() continually updates the display to show the new positions 
-			# of game elements and hides the old ones, creating the illusion of smooth movement.
-			pygame.display.flip()
+	def _check_events(self):  
+		"""Helper method to isolate the event management loop"""
+		# Watch for keyboard and mouse events - this loop listens for events
+		for event in pygame.event.get():  # the event.get() function returns a list of events that have taken place since the last time it was called
+			if event.type == pygame.QUIT:  # a pygame.QUIT event is detected if the player clicks the game window's close button
+				sys.exit()
+
+	def _update_screen(self):
+		"""Redraw the screen during each pass through the loop, and flip to the new screen."""
+		self.screen.fill(self.settings.bg_color)	# Fill the screen with the background colour using the fill() method
+		self.ship.blitme() # Draw the ship on the screen so it appears on top of the background	
+
+		# Make the most recently drawn screen visible. pygame.display.flip() continually updates the display to show the new positions 
+		# of game elements and hides the old ones, creating the illusion of smooth movement.
+		pygame.display.flip()
+
 
 
 if __name__ == '__main__':  # Specifies that the file should be called directly, making the following lines run
